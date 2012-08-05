bae3225054b3546b267aed227465690321e7f421
m4rienf <frank@apsu.be>
2012-08-05 19:35:53 +0200

    cleaned up coven coven logging to syslog

--- coven.py
+++ coven.py
@@ -1,24 +1,25 @@
-#!/usr/bin/python3 -u
-from time import sleep
-from dirmanager import DirManager
-from os import path
-from subprocess import Popen,PIPE
-from threading import Thread
-import re,traceback,sys,signal
-from extremon import CauldronReceiver,CauldronSender
+#!/usr/bin/python3 
+from dirmanager   import DirManager
+from os           import path
+from subprocess   import Popen,PIPE
+from threading    import Thread
+from queue        import Queue,Empty
+from extremon     import CauldronReceiver,CauldronSender
+import re,traceback,sys,signal,syslog
 import cProfile
 
 # ------------------------------------------------------------------------
 
-class CauldronToWitch(Thread):
-  def __init__(self,cauldronAddr,witch,inFilter,outFilter=None):
-    Thread.__init__(self)
+class CauldronToPlugin(Thread):
+  def __init__(self,name,cauldronAddr,plugin,inFilter,log,outFilter=None):
+    Thread.__init__(self,name="%s (in)" % (name))
     self.daemon=True
     self.cauldronAddr=cauldronAddr
-    self.witch=witch
+    self.plugin=plugin
     self.allowCache={}
     self.buffer=[]
     self.inFilter=re.compile(inFilter)
+    self.log=log
 
     if outFilter:
       self.outFilter=re.compile(outFilter) 
@@ -34,6 +35,7 @@ class CauldronToWitch(Thread):
   def handle_shuttle(self,shuttle):
     if not self.running:
       return
+
     self.buffer=[]
     for (label,value) in shuttle:
       try:
@@ -45,36 +47,37 @@ class CauldronToWitch(Thread):
         self.allowCache[label]=allow
       if allow:
         self.buffer.append("%s=%s" % (label,value))
+    self.buffer.append("");
 
     try:
-      self.witch.write(bytes('\n'.join(self.buffer),'utf-8'))
-      self.witch.write(bytes('\n','utf-8'))
+      self.plugin.write(bytes('\n'.join(self.buffer),'utf-8'))
     except IOError:
-      self.stop()
+      self.running=False
 
   def stop(self):
     self.running=False
-    self.witch.close()
+    self.plugin.close()
 
 # ------------------------------------------------------------------------
 
-class WitchToCauldron(Thread):
-  def __init__(self,cauldronAddr,witch,outFilter):
-    Thread.__init__(self)
+class PluginToCauldron(Thread):
+  def __init__(self,name,cauldronAddr,plugin,log,outFilter):
+    Thread.__init__(self,name="%s (out)" % (name))
     self.daemon=True
     self.cauldronAddr=cauldronAddr
-    self.witch=witch
+    self.plugin=plugin
     self.allowCache={}
     self.buffer=[]
     self.outFilter=re.compile(outFilter)
+    self.log=log
 
   def run(self):
     self.running=True
     self.cauldron=CauldronSender(self.cauldronAddr)
     while self.running:
-      data=self.witch.readline(256)
+      data=self.plugin.readline(256)
       if len(data)==0:
-        print("EOF from Plugin. Ending WitchToCauldron Thread")
+        self.log("%s: EOF From Plugin" % (self.name))
         self.running=False
       else:
         line=str(data,'utf-8')
@@ -90,42 +93,41 @@ class WitchToCauldron(Thread):
               if allow:
                 self.cauldron.put(label,value)
           except ValueError:
-            print("Can't Parse [%s]" % (line))
+            self.log("%s: Can't Parse [%s]" % (self.name,line))
   
   def stop(self):
     self.running=False
-    self.witch.close()
+    self.plugin.close()
 
 # ------------------------------------------------------------------------
 
-class Witch(Thread):
-  def __init__(self,name,path,prefix,config,cauldronAddr):
+class Plugin(Thread):
+  def __init__(self,name,path,prefix,config,cauldronAddr,log):
     Thread.__init__(self,name='%s' % (name))
     self.deamon=True
     self.process=None
     self.c2p=None
     self.p2c=None
     self.running=False
-    self.name=name
     self.path=path
     self.prefix=prefix
     self.config=config
+    self.log=log
     self.cauldronAddr=cauldronAddr
     self.isInput=(self.config.get('in.filter')!=None)
     self.isOutput=(self.config.get('out.filter')!=None)
-
     if self.isInput:
       if self.isOutput:
-        print("%s: in(%s)/out(%s)" % (self.name,
+        self.log("%s: in(%s)/out(%s)" % (self.name,
                                       self.config.get('in.filter'),
                                       self.config.get('out.filter')))
       else:
-        print("%s: in(%s)" % (self.name,self.config.get('in.filter')))
+        self.log("%s: in(%s)" % (self.name,self.config.get('in.filter')))
     else:
-       print("%s: out(%s)" % (self.name,self.config.get('out.filter')))
+       self.log("%s: out(%s)" % (self.name,self.config.get('out.filter')))
 
   def run(self):
-    print("%s: Starting.." % (self.name))
+    self.log("%s: Starting.." % (self.name),priority=syslog.LOG_INFO)
     self.running=True
     try:
       self.process=Popen( self.path,
@@ -134,51 +136,55 @@ class Witch(Thread):
                           stdout= PIPE if self.isOutput else None,
                           stderr= PIPE)
 
-      print("%s Started" % (self.name))
-
       if 'in.filter' in self.config:
-        self.c2p=CauldronToWitch(self.cauldronAddr,
+        self.c2p=CauldronToPlugin(self.name,self.cauldronAddr,
                                  self.process.stdin,
                                  inFilter=self.config.get('in.filter'),
+                                 log=self.log,
                                  outFilter=self.config.get('out.filter'))
         self.c2p.start()
 
       if 'out.filter' in self.config:
-        self.p2c=WitchToCauldron(self.cauldronAddr,
+        self.p2c=PluginToCauldron(self.name,self.cauldronAddr,
                                  self.process.stdout,
+                                 log=self.log,
                                  outFilter=self.config.get('out.filter'))
         self.p2c.start()
 
       while self.running:
         data=self.process.stderr.read()
         if len(data)>0:
+          lines=['%s reports:' % (self.name)]
           for error_line in str(data,'UTF-8').split('\n'):
-            print("%s: %s" % (self.name,error_line))
-          sys.stderr.flush()
+            lines.append(" | %s" % (error_line))
+          lines.append("")
+          self.log(lines)
         else:
-          print("%s: Saw EOF On STDERR" % (self.name))
+          self.log("%s: EOF On stderr" % (self.name))
           self.running=False
     except Exception as ex:
-      traceback.print_exc()
       self.running=False
 
   def stop(self):
-    print("%s: Asked To Stop" % (self.name))
+    self.log("%s: Asked To Stop" % (self.name))
     self.running=False
     if self.p2c:
       self.p2c.stop()
     if self.c2p:
       self.c2p.stop()
+    self.log("%s: Stopped" % (self.name),priority=syslog.LOG_INFO)
 
 # ------------------------------------------------------------------------
 
 class Coven(object):
   def __init__( self,path,prefix,cauldronAddr,
                 ignore=[r'^\.',r'\.x?swp$',r'~']):
+    syslog.openlog(ident="X3Coven",facility=syslog.LOG_DAEMON)
     self.path=path
     self.prefix=prefix
     self.cauldronAddr=cauldronAddr
-    self.witches={}
+    self.plugins={}
+    self.logq=Queue()
     self.confRE=re.compile('^#\\s*x3\.([a-z0-9.]+)\\s*=\\s*(.*)\\s*$')
     self.dirManager=DirManager(self.path,self,ignore=ignore)
     self.cauldron=CauldronSender(self.cauldronAddr)
@@ -197,57 +203,88 @@ class Coven(object):
     return False
 
   def getConfig(self,name):
-    witchPath=self.getOSPath(name)    
-    if not self.isShebangExecutable(witchPath):
+    pluginPath=self.getOSPath(name)    
+    if not self.isShebangExecutable(pluginPath):
       return None
     conf={}
     try:
-      with open(witchPath,'r') as plugin_file:
+      with open(pluginPath,'r') as plugin_file:
         for line in plugin_file:
           config_match=self.confRE.match(line)
           if config_match:
             conf[config_match.group(1)]=config_match.group(2)
-    except Exception as ex:
-      traceback.print_exc()
+    except:
       return None
     return conf
+  
+  def log(self,message,priority=syslog.LOG_DEBUG):
+    self.logq.put((priority,message))
+
+  def deQueueLogQueue(self,block=True):
+    try:
+      while True:
+        (priority,message)=self.logq.get(block=block)
+        print(priority)
+        if type(message)==str:
+          syslog.syslog(priority,message)
+        elif type(message)==list:
+          for line in message:
+            syslog.syslog(priority,line)
+        self.logq.task_done()
+    except Empty:
+      pass
 
   def practice(self):
     try:
       self.dirManager.start()
-      while True:
-        sleep(1)
+      self.deQueueLogQueue(block=True)
     except KeyboardInterrupt:
-      print("Coven stopping..")
+      self.log("Coven stopping..")
       self.dirManager.stop()
-      for partingWitch in self.witches.values():
-        partingWitch.stop()
-      
+      for partingPlugin in self.plugins.values():
+        partingPlugin.stop()
+      self.deQueueLogQueue(block=False) 
+      self.log("Coven stopped..",priority=syslog.LOG_INFO)
+
+# ------------------------------------------------------------------------
+
+  def startPlugin(self,name):
+    self.log("Starting [%s]" % (name)) 
+    config=self.getConfig(name)
+    if config:
+      try:
+        self.plugins[name]=Plugin(name, self.getOSPath(name),
+                                 self.prefix, config, self.cauldronAddr,
+                                 self.log)
+        self.plugins[name].start()
+        self.log("%s Started" % (name),priority=syslog.LOG_INFO)
+      except:
+        self.log("%s Failed To Start" % (name),priority=syslog.LOG_ERR)
+    else:
+      self.log("%s Is Not an X3 Plugin" % (name),
+               priority=syslog.LOG_WARNING)
+
+  def stopPlugin(self,name):
+    self.log("Stopping [%s]" % (name))
+    try:
+      self.plugins[name].stop()
+      del self.plugins[name]
+      self.log("%s Stopped" % (name),priority=syslog.LOG_INFO) 
+    except:
+      self.log("%s Failed To Stop" % (name),priority=syslog.LOG_ERR) 
 
 # ------------------------------------------------------------------------
 
   def process_FileCreated(self,name):
-    print("%s arrives" % (name)) 
-    config=self.getConfig(name)
-    if config:
-      self.witches[name]=Witch( name,
-                                self.getOSPath(name),
-                                self.prefix,
-                                config,
-                                self.cauldronAddr)
-      self.witches[name].start()
+    self.startPlugin(name)
 
   def process_FileDeleted(self,name):
-    print("%s leaves" % (name)) 
-    try:
-      self.witches[name].stop()
-      del self.witches[name]
-    except KeyError:
-      pass
+    self.stopPlugin(name)
 
   def process_FileChanged(self,name):
-    self.process_FileDeleted(name)
-    self.process_FileCreated(name)
+    self.log("Restarting [%s]" % (name),priority=syslog.LOG_INFO) 
+    self.stopPlugin(name)
+    self.startPlugin(name)
   
 # ------------------------------------------------------------------------
 
