c84a0b1387691ee339f03e1bb9a0f89bdff9d4c0
Frank Marien <frank@apsu.be>
2012-08-14 12:59:15 +0200

    forcing formatted extremon.py

--- extremon.py
+++ extremon.py
@@ -17,7 +17,7 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with ExtreMon.  If not, see <http://www.gnu.org/licenses/>.
-#
+# 
 
 import time,socket, struct
 from queue      import Queue
